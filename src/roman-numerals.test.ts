9b8ce55d8300bdb78ac30b3bc73185130d70c9a9
LALLEMAND Theophile <theophile.lallemand9@etu.univ-lorraine.fr>
2025-05-23 11:25:08 +0200

    red

--- src/roman-numerals.test.ts
+++ src/roman-numerals.test.ts
@@ -1,1 +1,16 @@
+import { expect, test } from "vitest";
+
+class RomanNumeral {
+  of(value: string): number {
+    
+  }
+}
+
+
+test.each([
+    ["I", 1],
+])("%s should give %s", (input, expected) => {
+  expect(new RomanNumeral().of(input)).toBe(expected);
+});
+
 // Write your test here
