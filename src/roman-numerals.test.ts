2374b07eae7068061f88eaebcfbb8442c0d9c3ce
LALLEMAND Theophile <theophile.lallemand9@etu.univ-lorraine.fr>
2025-05-23 11:33:09 +0200

    green valeur I  = 1 et red pour valeur II = 2

--- src/roman-numerals.test.ts
+++ src/roman-numerals.test.ts
@@ -3,6 +3,7 @@ import { expect, test } from "vitest";
 class RomanNumeral {
   of(value: string): number {
     if(value = "I") return 1
+    else if(value = "II") return 2
     else return 0
   }
 }
@@ -10,6 +11,7 @@ class RomanNumeral {
 
 test.each([
     ["I", 1],
+    ["II", 2],
 ])("%s should give %s", (input, expected) => {
   expect(new RomanNumeral().of(input)).toBe(expected);
 });
