33c4acd256ca42a483556e8b48a5f14a438a384c
LALLEMAND Theophile <theophile.lallemand9@etu.univ-lorraine.fr>
2025-05-26 15:02:34 +0200

    version corrigé

--- src/roman-numerals.test.ts
+++ src/roman-numerals.test.ts
@@ -1,22 +1,60 @@
-import { expect, test } from "vitest";
+import { test, expect } from 'vitest';
 
 class RomanNumeral {
+  private symbolMap = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+
+  private isLowerThan(romanianToTest: string, higherRomanian: string) {
+    return this.symbolMap[romanianToTest] < this.symbolMap[higherRomanian];
+  }
+
+  private valueOf(romanian: string) {
+    return this.symbolMap[romanian];
+  }
+
   of(value: string): number {
-    if(value === "I") return 1
-    else if(value === "II") return 2
-    else if(value === "III") return 3
-    else if(value === "V") return 5
-    else return 0
+    let index = 0;
+    let resultat = 0;
+    while (value[index]) {
+      const romanianChar = value[index];
+      const nextRomanianChar = value[index + 1];
+
+      if (this.isLowerThan(romanianChar, nextRomanianChar)) {
+        resultat += this.valueOf(nextRomanianChar) - this.valueOf(romanianChar);
+        index += 2;
+      } else if (this.valueOf(romanianChar)) {
+        resultat += this.valueOf(romanianChar);
+        index += 1;
+      } else throw new Error('Not a Romanian number');
+    }
+
+    return resultat;
   }
 }
 
 test.each([
-    ["I", 1],
-    ["II", 2],
-    ["III",3],
-    ["V",5]
-])("%s should give %s", (input, expected) => {
+  ['I', 1],
+  ['II', 2],
+  ['III', 3],
+  ['V', 5],
+  ['VI', 6],
+  ['VII', 7],
+  ['VIII', 8],
+  ['X', 10],
+  ['L', 50],
+  ['C', 100],
+  ['D', 500],
+  ['M', 1000],
+  ['IV', 4],
+  ['IX', 9],
+  ['CM', 900],
+])('%s should become %s', (input, expected) => {
   expect(new RomanNumeral().of(input)).toBe(expected);
 });
-
-// Write your test here
